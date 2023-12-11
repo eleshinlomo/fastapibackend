4d12915f735cebd40ab6100b1cbd56d1a033dc52
eleshinlomo <seun.olatunji2@gmail.com>
2023-12-11 12:06:58 -0500

    audio

--- main.py
+++ main.py
@@ -26,10 +26,11 @@ app = FastAPI()
 
 # CORS - Origins
 origins = [
-    "http://localhost:5173",
-    "http://localhost:5174",
-    "http://localhost:4173",
-    "http://localhost:3000",
+    "*"
+    # "http://localhost:5173",
+    # "http://localhost:5174",
+    # "http://localhost:4173",
+    # "http://localhost:3000",
 ]
 
 

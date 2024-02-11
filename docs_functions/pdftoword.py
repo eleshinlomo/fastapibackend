6e5849d0eb246bec61b52693f91ae5343dffd569
eleshinlomo <seun.olatunji2@gmail.com>
2024-02-11 16:13:23 -0500

    mm

--- docs_functions/pdftoword.py
+++ docs_functions/pdftoword.py
@@ -1,14 +1,14 @@
-from pdf2docx import Converter
-from io import BytesIO
-from fastapi import FastAPI, File, UploadFile, HTTPException
+# from pdf2docx import Converter
+# from io import BytesIO
+# from fastapi import FastAPI, File, UploadFile, HTTPException
 
 
-def convert_pdf_to_docx(pdf):
-    try:
-        cv = Converter(pdf)
-        docx_stream = BytesIO()
-        cv.convert(docx_stream, start=0, end=None)
-        docx_stream.seek(0)
-        return docx_stream.read()
-    except Exception as e:
-        raise HTTPException(status_code=500, detail=str(e))
+# def convert_pdf_to_docx(pdf):
+#     try:
+#         cv = Converter(pdf)
+#         docx_stream = BytesIO()
+#         cv.convert(docx_stream, start=0, end=None)
+#         docx_stream.seek(0)
+#         return docx_stream.read()
+#     except Exception as e:
+#         raise HTTPException(status_code=500, detail=str(e))
